5c56ac9e52a06492d5f05f8c6a22bf78ffbfc59a
Aditya <adityagupta2791@gmail.com>
2025-10-26 12:58:57 +0530

    Everythig is working as of now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './components/Header.jsx'
 import Hero from "./components/Hero.jsx"
+import Navbar from "./components/Navbar.jsx"
 
 export default function App() {
 
   return (
     <div className="bg-[#0d1117] text-white font-[Inter]">
-      <Header />
+      <Navbar />
       <Hero />
     </div>
   )
