71e0f9f8310e600c028b5f458bca0f994b15ae4b
Aditya <adityagupta2791@gmail.com>
2025-11-14 21:18:59 +0530

    updated Divider

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Divider = () => {
   return (
-    <div className="w-80 h-1 mx-auto my-10 bg-gradient-to-r from-orange-400/20 via-orange-400 to-orange-400/20 rounded-full shadow-[0_0_15px_rgba(251,146,60,0.4)]" />
-  )
-}
+    <div className="w-full h-[2px] mx-auto bg-gradient-to-r from-transparent via-orange-400 to-transparent rounded-full shadow-[0_0_12px_rgba(251,146,60,0.35)] animate-pulse" />
+  );
+};
 
 export default Divider
